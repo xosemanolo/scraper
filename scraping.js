f583a49d3a1b0fd0076ce94215ca827f09a99cf8
xosemanolo <xose.manolo.rodriguez@gmail.com>
2020-02-09 21:29:36 -0500

    implement cron - node scheduler & add env plus gitignore file

--- scraping.js
+++ scraping.js
@@ -1,50 +1,37 @@
+const cron = require('node-cron');
 const axios = require('axios');
-// let twilio = require('twilio');
 const cheerio = require('cheerio');
+require('dotenv').config({path: __dirname + '/.env'})
 
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 let message;
+let task = cron.schedule('* */23 * * *', () => {
+    getLinkDetails = async () => {
+        console.log('first')
+            let res = await axios.get('https://careers.linkedin.com/reach/howtoapply')
+            // console.log(res)
+                const $ = cheerio.load(res.data);
+                message = $('.banner-subheadline').text();
+                console.log('second',message) 
+             await client.messages.create({
+                to: process.env.CELL_PHONE_NUMBER,
+                from: process.env.TWILIO_PHONE_NUMBER,
+                body: message
+            })
+            .then(message => console.log(message.sid))
+            .catch(err => {
+                console.log(err);
+                return null;
+            });
+            console.log('done')
+    }
+    getLinkDetails()
+})
 
-getLinkDetails = async () => {
-    console.log('first')
-        let res = await axios.get('https://careers.linkedin.com/reach/howtoapply')
-        // console.log(res)
-            const $ = cheerio.load(res.data);
-            message = $('.banner-subheadline').text();
-            console.log('second',message) 
-         await client.messages.create({
-            to: process.env.CELL_PHONE_NUMBER,
-            from: process.env.TWILIO_PHONE_NUMBER,
-            body: message
-        })
-        // .then(message => console.log(message.sid));
-        .catch(err => {
-            console.log(err);
-            return null;
-        });
-        console.log('done')
-}
-getLinkDetails()
-
-// axios.get('https://careers.linkedin.com/reach/howtoapply').then((res) => {
-    
-// })
-
+task.start()
 
 
-
-
-
-// .then((resp) => {
-//     console.log(resp)
-//     client.messages.create({
-//         to: process.env.TWILIO_PHONE_NUMBER,
-//         from: process.env.CELL_PHONE_NUMBER,
-//         body: message
-//     })
-// })
-// .then(message => console.log(message.sid));
 
 
 
