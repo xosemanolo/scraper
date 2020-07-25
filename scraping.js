64d0cde105086f1abf6e56f2ae5300b9ec9a612b
xosemanolo <xose.manolo.rodriguez@gmail.com>
2020-07-25 18:13:43 -0400

    redeploying

--- scraping.js
+++ scraping.js
@@ -1,34 +1,34 @@
 // const cron = require('node-cron');
+//
 const axios = require('axios');
 const cheerio = require('cheerio');
-require('dotenv').config({path: __dirname + '/.env'})
+require('dotenv').config({ path: __dirname + '/.env' });
 
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 let message;
 
-    getLinkDetails = async () => {
-        console.log('first')
-            let res = await axios.get('https://careers.linkedin.com/reach/howtoapply')
-            // console.log(res)
-                const $ = cheerio.load(res.data);
-                message = $('.banner-subheadline').text();
-                console.log('second',message) 
-             await client.messages.create({
-                to: process.env.CELL_PHONE_NUMBER,
-                from: process.env.TWILIO_PHONE_NUMBER,
-                body: message
-            })
-            .then(message => console.log(message.sid))
-            .catch(err => {
-                console.log(err);
-                return null;
-            });
-            console.log('done')
-    }
-    getLinkDetails()
-
-
-
-
-
+getLinkDetails = async () => {
+  console.log('first');
+  let res = await axios.get('https://careers.linkedin.com/reach/howtoapply');
+  // console.log(res)
+  const $ = cheerio.load(res.data);
+  message = $('.banner-subheadline').text();
+  console.log('second', message);
+  await client.messages
+    .create({
+      to: process.env.CELL_PHONE_NUMBER,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      body: message,
+    })
+    .then((message) => console.log(message.sid))
+    .catch((err) => {
+      console.log(err);
+      return null;
+    });
+  console.log('done');
+};
+getLinkDetails();
